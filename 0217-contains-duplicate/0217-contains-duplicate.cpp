c30e554f6e6d0ffe28a8f824ae5a9c738643cee4
abdulrehmanashfaq <83540709+abdulrehmanashfaq@users.noreply.github.com>
2026-01-17 22:18:31 +0500

    Time: 17 ms (95.24%), Space: 61.3 MB (100%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int i=0;
+        int j=i+1;
+        while(j<n){
+            if(nums[i]==nums[j]){
+                return true ;
+            }
+            i++;
+            j++;
+        }
+        return false;
+    }
+};
